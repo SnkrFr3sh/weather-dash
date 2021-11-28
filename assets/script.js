8f277440862b2fb22c898c4b972134ff5651d956
SnkrFr3sh <mpacampara@gmail.com>
2021-11-27 21:27:34 -0800

    added icons and cleaned up comments

--- assets/script.js
+++ assets/script.js
@@ -18,6 +18,7 @@ recentSearchListItem = $('.recentSearchListItem');
 listItem1 = $('.listItem1');
 listItem2 = $('.listItem2');
 listItem3 = $('.listItem3');
+currentWeatherIcon = $('#currentWeatherIcon')
 
 
 
@@ -30,74 +31,45 @@ var day4 = today.add(1, 'days').format("MMM Do");
 var day5 = today.add(1, 'days').format("MMM Do");
 var day6 = today.add(1, 'days').format("MMM Do");
 
-day2Date=$('.day2Card');
-day3Date=$('.day3Card');
-day4Date=$('.day4Card');
-day5Date=$('.day5Card');
-day6Date=$('.day6Card');
+day2Date = $('.day2Card');
+day3Date = $('.day3Card');
+day4Date = $('.day4Card');
+day5Date = $('.day5Card');
+day6Date = $('.day6Card');
 
 
 userCity = $('.userCity')
 searchBtn = $('.searchBtn')
 var recentSearches = []
 
-function searchHistory(){
-    if (recentSearches !== 0)
-    for (let i = 0; i < recentSearches.length; i++) {
-        var listItem = document.createElement('li');
-        listItem.textContent = recentSearches[i];
-        recentSearchList.append(listItem);
-    } console.log('test search history')
-};
+
 
 
 
 searchBtn.on('click', function () {
 
-    // var searchResult = requestUrl + userCity.val() + '&2Cus&units=imperial';
-    // localStorage.setItem('searchResult', searchResult)
-    // console.log('searchResult',searchResult);
-   
-
-    // var currentCity = currentCityUrl + userCity.val() + '&days=5';
-    // console.log('currentCity',currentCity)
-    // localStorage.setItem('currentCity', currentCity)
-    
-
     recentSearches.push(userCity.val());
-    // console.log("recentSearches", recentSearches)
+
     localStorage.setItem('recentSearches', recentSearches)
 
-    for (let i = 0; i < recentSearches.length; i++) {
-        listItem1.textContent = recentSearches[0];
-        listItem2.textContent = recentSearches[1];
-        listItem3.textContent = recentSearches[2];
-        recentSearchListItem.append(listItem1);
-        recentSearchListItem.append(listItem2);
-        recentSearchListItem.append(listItem3);
-    }
     currentCityResult();
     fiveDayForecast();
 
+
 })
 
-// function searchList(){
-//     for (let i = 0; i < recentSearches.length; i++) {
-//         var listItem = document.createElement('li');
-//         listItem.textContent = recentSearches[i];
-//         recentSearchList.append(listItem);
-// }
-
-// searchList();
-
-// var searchResult = 'searchResult';
-
-// searchResult = localStorage.getItem('searchResult');
+function searchList() {
+    for (let i = 0; i < recentSearches.length; i++) {
+        var listItem = document.createElement('li');
+        listItem.textContent = recentSearches[i];
+        recentSearchList.append(listItem);
+    }
+}
 
 function fiveDayForecast() {
     var searchResult = requestUrl + userCity.val() + '&2Cus&units=imperial';
     localStorage.setItem('searchResult', searchResult)
-    console.log('searchResult',searchResult);
+    console.log('searchResult', searchResult);
     fetch(searchResult, {
         "method": "GET",
         "headers": {
@@ -119,70 +91,71 @@ function fiveDayForecast() {
             day5Date.text(day5);
             day6Date.text(day6);
             // day2
-            day2Icon=$('<img> ')
-            day2Temp=$('<p>')
-            day2wind=$('<p>')
-            day2humidity=$('<p>')
-            day2Icon.text('http://openweathermap.org/img/w/'+data.list[1].weather[0].icon+'.png>')
-            day2Temp.text(data.list[1].main.temp +'℉')
-            day2wind.text(data.list[1].wind.speed +'mph')
-            day2humidity.text(data.list[1].main.humidity +'%' )
+            day2Icon = $('<img> ')
+            day2Temp = $('<p>')
+            day2wind = $('<p>')
+            day2humidity = $('<p>')
+            day2Icon.attr('src', 'http://openweathermap.org/img/w/' + data.list[1].weather[0].icon + '.png')
+            day2Temp.text(data.list[1].main.temp + '℉')
+            day2wind.text(data.list[1].wind.speed + 'mph')
+            day2humidity.text(data.list[1].main.humidity + '%')
             day2Date.append(day2Icon)
             day2Date.append(day2Temp)
             day2Date.append(day2wind)
             day2Date.append(day2humidity)
             // day3
-            day3Icon=$('<img>')
-            day3Temp=$('<p>')
-            day3wind=$('<p>')
-            day3humidity=$('<p>')
-            day3Icon.text(data.list[8].weather[0].icon)
+            day3Icon = $('<img>')
+            day3Temp = $('<p>')
+            day3wind = $('<p>')
+            day3humidity = $('<p>')
+            day3Icon.attr('src', 'http://openweathermap.org/img/w/' + data.list[8].weather[0].icon + '.png')
             day3Temp.text(data.list[8].main.temp + '℉')
-            day3wind.text(data.list[8].wind.speed +'mph')
-            day3humidity.text(data.list[8].main.humidity +'%' )
+            day3wind.text(data.list[8].wind.speed + 'mph')
+            day3humidity.text(data.list[8].main.humidity + '%')
             day3Date.append(day3Icon)
             day3Date.append(day3Temp)
             day3Date.append(day3wind)
             day3Date.append(day3humidity)
             // day4
-            day4Icon=$('<img>')
-            day4Temp=$('<p>')
-            day4wind=$('<p>')
-            day4humidity=$('<p>')
-            day4Icon.text(data.list[16].weather[0].icon)
+            day4Icon = $('<img>')
+            day4Temp = $('<p>')
+            day4wind = $('<p>')
+            day4humidity = $('<p>')
+            day4Icon.attr('src', 'http://openweathermap.org/img/w/' + data.list[16].weather[0].icon + '.png')
             day4Temp.text(data.list[16].main.temp + '℉')
-            day4wind.text(data.list[16].wind.speed +'mph')
-            day4humidity.text(data.list[16].main.humidity +'%' )
+            day4wind.text(data.list[16].wind.speed + 'mph')
+            day4humidity.text(data.list[16].main.humidity + '%')
             day4Date.append(day4Icon)
             day4Date.append(day4Temp)
             day4Date.append(day4wind)
             day4Date.append(day4humidity)
             // day5
-            day5Icon=$('<img>')
-            day5Temp=$('<p>')
-            day5wind=$('<p>')
-            day5humidity=$('<p>')
-            day5Icon.text(data.list[24].weather[0].icon)
+            day5Icon = $('<img>')
+            day5Temp = $('<p>')
+            day5wind = $('<p>')
+            day5humidity = $('<p>')
+            day5Icon.attr('src', 'http://openweathermap.org/img/w/' + data.list[24].weather[0].icon + '.png')
             day5Temp.text(data.list[24].main.temp + '℉')
-            day5wind.text(data.list[24].wind.speed +'mph')
-            day5humidity.text(data.list[24].main.humidity +'%' )
+            day5wind.text(data.list[24].wind.speed + 'mph')
+            day5humidity.text(data.list[24].main.humidity + '%')
             day5Date.append(day5Icon)
             day5Date.append(day5Temp)
             day5Date.append(day5wind)
             day5Date.append(day5humidity)
             // day6
-            day6Icon=$('<img>')
-            day6Temp=$('<p>')
-            day6wind=$('<p>')
-            day6humidity=$('<p>')
-            day6Icon.text(data.list[32].weather[0].icon)
+            day6Icon = $('<img>')
+            day6Temp = $('<p>')
+            day6wind = $('<p>')
+            day6humidity = $('<p>')
+            day6Icon.attr('src', 'http://openweathermap.org/img/w/' + data.list[32].weather[0].icon + '.png')
             day6Temp.text(data.list[32].main.temp + '℉')
-            day6wind.text(data.list[32].wind.speed +'mph')
-            day6humidity.text(data.list[32].main.humidity +'%' )
+            day6wind.text(data.list[32].wind.speed + 'mph')
+            day6humidity.text(data.list[32].main.humidity + '%')
             day6Date.append(day6Icon)
             day6Date.append(day6Temp)
             day6Date.append(day6wind)
             day6Date.append(day6humidity)
+            // console.log('icon test', list[0].weather[0].icon);
 
         })
 
@@ -193,9 +166,9 @@ function fiveDayForecast() {
 // currentCity = localStorage.getItem('currentCity');
 // cityResult = $('#citySearched');
 
-function currentCityResult() { 
+function currentCityResult() {
     var currentCity = currentCityUrl + userCity.val() + '&days=5';
-    console.log('currentCity',currentCity)
+    console.log('currentCity', currentCity)
     localStorage.setItem('currentCity', currentCity)
     fetch(currentCity, {
         "method": "GET",
@@ -212,14 +185,10 @@ function currentCityResult() {
         .then(function (data) {
             console.log(data);
             console.log('city name', data.location.name)
-            // cityResult = $('<p>');
-            // currentWeatherIcon = $('<p>');
-            // currentTemp = $('<p>');
-            // currentHumidity = $('<p>');
-            // currentUV = $('<p>');
-            // currentWindSpeed = $('<p>');
-            cityResult.text('City:' + data.location.name);
-            currentWeatherIcon.text('http://openweathermap.org/img/w/'+ data.list[1].weather[0].icon+'.png>')
+
+            cityResult.text('City: ' + data.location.name);
+
+            currentWeatherIcon.attr("src", 'http:' + data.current.condition.icon);
             currentTemp.text('Current Temp: ' + data.current.temp_f + ' ℉');
             currentHumidity.text('Humidity: ' + data.current.humidity);
             currentUV.text('UV index: ' + data.current.uv);
@@ -233,3 +202,5 @@ function currentCityResult() {
 
         })
 }
+
+searchList()
